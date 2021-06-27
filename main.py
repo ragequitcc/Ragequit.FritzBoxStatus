d62cf3c644d36ff39c460cd093bec3d95b9965ad
jerptrs <jerptrs@gmail.com>
2021-06-27 16:05:28 +0000

    added v6 support

--- main.py
+++ main.py
@@ -3,32 +3,26 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 from fritzconnection.lib.fritzstatus import FritzStatus
 from fritzconnection import FritzConnection
 
-if "FritzBoxUri" not in environ:
-    print("FritzBoxUri Missing")
-    quit()
-
-if "FritzBoxUser" not in environ:
-    print("FritzBoxUser Missing")
-    quit()
-
-if "FritzBoxPassword" not in environ:
-    print("FritzBoxPassword Missing")
-    quit()
-
-fc = FritzConnection(address=environ["FritzBoxUri"],
-                     user=environ["FritzBoxUser"], password=environ["FritzBoxPassword"])
+fc = FritzConnection(address="192.168.178.1",
+                     user="dyndns", password="admin123")
 fs = FritzStatus(fc)
 
 class Server(BaseHTTPRequestHandler):
     def do_GET(self):
-        if self.path == "/ip":
-            ip = fs.external_ip if fs.external_ip else fc.call_action("WANPPPConnection1", "GetInfo")["NewExternalIPAddress"]
-            
+        ip = fs.external_ip if fs.external_ip else fc.call_action("WANPPPConnection1", "GetInfo")["NewExternalIPAddress"]
+        if self.path == "/ip/v4":
             self.send_response(200)
             self.send_header("Content-type", "text/html")
             self.end_headers()
             self.wfile.write(ip.encode())
 
+        elif self.path == "/ip/v6":
+            ip = fs.external_ipv6 if fs.external_ipv6 else "Not Available"
+
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(ip.encode())
 
 httpd = HTTPServer(("0.0.0.0", 8080), Server)
 httpd.serve_forever()
